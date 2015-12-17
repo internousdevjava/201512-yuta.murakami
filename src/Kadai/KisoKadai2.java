d76d2f6c5c920bbb25704b5bb802a385a8ffb3d9
= <=>
2015-12-17 12:36:01 +0900

    基礎課題1&2提出します。

--- src/Kadai/KisoKadai2.java
+++ src/Kadai/KisoKadai2.java
@@ -6,34 +6,52 @@ import java.io.InputStreamReader;
 
 public class KisoKadai2 {
 	public static void main(String[] args) {
-		int answer = new java.util.Random().nextInt(100);
-		for (;;) {
-			System.out.println("0から100までの整数を入力してください。");
-			// 以前のやり方→int num=new java.util.Scanner(System.in).nextInt();
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String num = null;
-			try {
-				num = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-				break;
-			}
-			int num1 = Integer.parseInt(num);
-			// System.out.println(ans);
-			// checkStringNumberを使えるようにしたい
 
-			if (num1 == answer) {
-				System.out.println("正解！答えは" + answer + "です！");
-				break;
-			} else if (num1 < answer - 10) {
-				System.out.println("小さい！");
-			} else if (num1 < answer && num1 > answer - 10) {
-				System.out.println("ちょっと小さい……。");
-			} else if (num1 > answer + 10) {
-				System.out.println("大きい！");
-			} else if (num1 > answer && num1 < answer + 10) {
-				System.out.println("ちょっと大きい……。");
+		for (;;) {
+			int answer = new java.util.Random().nextInt(100);
+			while(true){
+				System.out.println("0から100までの整数を入力してください。");
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				String num = null;
+				try {
+					num = br.readLine();
+					int numli = Integer.parseInt(num);
+					numlimit(numli);
+					boolean numcheck = numlimit(numli);
+					if (numcheck) {
+					} else {
+						System.out.println("桁が大きすぎます。");
+						continue;
+					}
+					if (numli == answer) {
+						System.out.println("正解！答えは" + answer + "です！");
+						break;
+					} else if (numli < answer - 10) {
+						System.out.println("小さい！");
+					} else if (numli < answer && numli > answer - 10) {
+						System.out.println("ちょっと小さい……。");
+					} else if (numli > answer + 10) {
+						System.out.println("大きい！");
+					} else if (numli > answer && numli < answer + 10) {
+						System.out.println("ちょっと大きい……。");
+					}
+				} catch (IOException e) {
+					System.out.println(e);
+					System.out.println("もう一度やってみてね。");
+					continue;
+				}catch (NumberFormatException e) {
+					System.out.println("整数の数字を入れてね。");
+					continue;
+				}
 			}
+
+		}
+	}
+	private static boolean numlimit(int numli) {
+		if (numli <= 100) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
